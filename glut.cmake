0f625608cd99404312405d0eeedb17c6e5ef6591
Ramin Raeisi <rr@epen.io>
2023-04-29 14:10:37 +0200

    Add version 0.1

---
+++ glut.cmake
@@ -0,0 +1,9 @@
+find_path(GLUT_INCLUDE GL/glut.h PATHS ${CMAKE_CURRENT_LIST_DIR})
+
+find_path( GLUT_LIB_DEBUG libfreeglut.lib PATHS ${CMAKE_CURRENT_LIST_DIR}/cmake64/debug)
+find_path( GLUT_LIB_RELEASE libfreeglut.lib PATHS ${CMAKE_CURRENT_LIST_DIR}/cmake64/release)
+set(Glut_LIBRARIES debug ${GLUT_LIB_DEBUG}/libfreeglut.lib optimized ${GLUT_LIB_RELEASE}/libfreeglut.lib)
+
+include_directories(${GLUT_INCLUDE})
+
+add_definitions(-DFREEGLUT_STATIC)
